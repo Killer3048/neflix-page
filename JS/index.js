15617bb499160199deb9eb7233a18114005d1354
Killer3048 <gamert11@mail.ru>
2022-12-20 00:47:00 +0700

    '

--- JS/index.js
+++ JS/index.js
@@ -18,3 +18,49 @@ document.getElementById("My_List").onclick = function() {
     document.getElementById("Movies").classList.remove("header__links_is-active")
 }
 
+$(document).ready(function(){
+    let sliderPosition = 0;
+    const sliderContainer = $('.carousel-wrapper');
+    const sliderTrack = $('.cards');
+    const sliderItem = $('.cards__component');
+    const sliderItemWidth = sliderItem.width();
+    const sliderContainerWidth = sliderContainer.width();
+    const sliderTrackWidth = sliderItem.length * sliderItemWidth - sliderContainerWidth + sliderContainerWidth/2; 
+    const sliderButtonPrev = $('.arrow');
+    const sliderButtonNext = $('.arrow-right');
+    sliderButtonPrev.on('click', function(){
+        sliderPosition += sliderItemWidth; 
+        if (sliderPosition > 0) {
+            sliderPosition = 0;
+        }
+        sliderTrack.css('transform', `translateX(${sliderPosition}px`);
+        sliderButtons();
+    });
+    sliderButtonNext.on('click', function(){
+        sliderPosition -= sliderItemWidth;
+        if (sliderPosition < -sliderTrackWidth) {
+            sliderPosition = -sliderTrackWidth;
+        }
+        sliderTrack.css('transform', `translateX(${sliderPosition}px`);
+        sliderButtons();
+    });
+    const sliderButtons = () => {
+        if (sliderPosition == 0) {
+            sliderButtonPrev.hide();
+        } else {
+            sliderButtonPrev.show();
+        }
+        if (sliderPosition == -sliderTrackWidth) {
+            sliderButtonNext.hide();
+        } else {
+            sliderButtonNext.show();
+        }
+
+     }
+
+    sliderButtons();
+});
+
+
+
+
